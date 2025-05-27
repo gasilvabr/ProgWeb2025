45229a9cdd26a5a93d5753e746686f1d9d31890c
gasilvabr <gasilvabr@hotmail.com>
2025-05-27 13:39:33 -0300

    Alterções na visão

--- Loja-g1/loja/views/ProdutoView.py
+++ Loja-g1/loja/views/ProdutoView.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from loja.models import Produto
+from datetime import timedelta, datetime
+from django.utils import timezone
 
 def list_produto_view(request, id=None):
     #Carrega dados do navegador
@@ -7,7 +9,8 @@ def list_produto_view(request, id=None):
     destaque = request.GET.get("destaque")
     promocao = request.GET.get("promocao")
     categoria = request.GET.get("categoria")
-    fabricante = request.GET.get("fabricante")
+    f = request.GET.get("xfabricante")
+    dias = request.GET.get("dias")
     #mostra dados do navegador
     if destaque is not None:
         print(destaque)
@@ -17,22 +20,29 @@ def list_produto_view(request, id=None):
         print(promocao)
     if categoria is not None:
         print(categoria)
-    if fabricante is not None:
-        print(fabricante)
+    if f is not None:
+        print(f)
     #carrega dados do banco de dados
     produtos = Produto.objects.all()
+    #produtos = Produto.objects.filter(criado_em__gt=datetime.now())
     if produto is not None:
-        produtos = produtos.filter(Produto=produto)
+        produtos = produtos.filter(Produto__contains=produto)
     if promocao is not None:
         produtos = produtos.filter(promocao=promocao)
     if destaque is not None:
         produtos = produtos.filter(destaque=destaque)
     if categoria is not None:
-        produtos = produtos.filter(categoria=categoria)
-    if fabricante is not None:
-        produtos = produtos.filter(fabricante=fabricante)
+        produtos = produtos.filter(categoria__Categoria=categoria)
+    if f is not None:
+        produtos = produtos.filter(fabricante__Fabricante=f)
     if id is not None:
         produtos = produtos.filter(id=id)
+    if dias is not None:
+        print(dias)
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gt=now)
+                
     #mostra dados do banco de dados
     print(produtos)
 
--- loja-g2/loja/views/ProdutoView.py
+++ loja-g2/loja/views/ProdutoView.py
@@ -1,3 +1,51 @@
 from django.http import HttpResponse
+from loja.models import Produto
+from datetime import timedelta, datetime
+from django.utils import timezone
+
 def list_produto_view(request, id=None):
+    # Carrega informações vindas do navegador (query string)
+    produto = request.GET.get("produto")
+    destaque = request.GET.get("destaque")
+    promocao = request.GET.get("promocao")
+    categoria = request.GET.get("categoria")
+    fabricante = request.GET.get("fabricante")
+    dias = request.GET.get("dias")
+    ## mostra no console
+    if destaque is not None:
+        print(destaque)
+    if produto is not None:
+        print(produto)
+    if promocao is not None:
+        print(promocao)
+    if categoria is not None:
+        print(categoria)
+    if fabricante is not None:
+        print(fabricante)
+
+    # Carrega informações vindas dobanco de dados
+    produtos = Produto.objects.all()
+    #produtos = Produto.objects.filter(id__in=[2,3])
+    #produtos = Produto.objects.filter(criado_em__gt=datetime.now())
+    if produto is not None:
+        produtos = produtos.filter(Produto__contains=produto )
+    if promocao is not None:
+        produtos = produtos.filter(promocao=promocao)
+    if destaque is not None:
+        produtos = produtos.filter(destaque=destaque)
+    if categoria is not None:
+        produtos = produtos.filter(categoria__Categoria=categoria)
+    if fabricante is not None:
+        produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    if dias is not None:
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
+    # mostra no console
+    print(produtos)
+
+    if id is None:
+        return HttpResponse('<h1>Nenhum id foi informado</h1>')
     return HttpResponse('<h1>Produto de id %s!</h1>' % id)
