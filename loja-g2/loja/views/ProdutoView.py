73b12faa66a0c1a77a6be77210937e4ca0e7e28d
gasilvabr <gasilvabr@hotmail.com>
2025-06-10 15:59:48 -0300

    tela de edição

--- loja-g2/loja/views/ProdutoView.py
+++ loja-g2/loja/views/ProdutoView.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+from django.shortcuts import render # Retire from django.http import HttpResponse
 
 def list_produto_view(request, id=None):
     # Carrega informações vindas do navegador (query string)
@@ -43,9 +44,20 @@ def list_produto_view(request, id=None):
         now = timezone.now()
         now = now - timedelta(days = int(dias))
         produtos = produtos.filter(criado_em__gte=now)
-    # mostra no console
-    print(produtos)
+        
+    # Adicione para definir o contexto e carregar o template
+    context = {
+        'produtos': produtos
+    }
+    return render(request, template_name='produto/produto.html', context=context, status=200)
 
-    if id is None:
-        return HttpResponse('<h1>Nenhum id foi informado</h1>')
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = { 
+        'produto': produto 
+    }
+    return render(request, template_name='produto/produto-edit.html', context=context, status=200)    
