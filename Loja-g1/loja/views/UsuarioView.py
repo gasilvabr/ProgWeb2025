51478356adb97544a49b67e3693aa0cf976fc472
gasilvabr <gasilvabr@hotmail.com>
2025-09-15 14:51:12 -0300

    cap 10 grupo 1

--- Loja-g1/loja/views/UsuarioView.py
+++ Loja-g1/loja/views/UsuarioView.py
@@ -13,10 +13,40 @@ def list_usuario_view(request, id=None):
 # adicione o método de edição
 def edit_usuario_view(request):
     usuario = get_object_or_404(Usuario, user=request.user)
-    usuarioForm = UserUsuarioForm(instance=usuario)
-    userForm = UserForm(instance=request.user)
+
+    # Adicione as linhas a seguir
+    # Perceba que os forms foram transferidos para dentro do if e do else
+    emailUnused = True
+    # Adicione a linha a seguir
+    message = None    
+    if request.method == 'POST':
+        usuarioForm = UserUsuarioForm(request.POST, instance=usuario)
+        userForm = UserForm(request.POST, instance=request.user)
+        # Verifica se o e-mail que o usuário está tentando utilizar
+        # em seu perfil já existe em outro perfil
+        verifyEmail = Usuario.objects.filter(user__email=request.POST['email']).exclude(user__id=request.user.id).first()
+        emailUnused = verifyEmail is None
+    else:
+        usuarioForm = UserUsuarioForm(instance=usuario)
+        userForm = UserForm(instance=request.user)
+    if usuarioForm.is_valid() and userForm.is_valid() and emailUnused:
+        usuarioForm.save()
+        userForm.save()
+        # Adicione a linha a seguir
+        message = { 'type': 'success', 'text': 'Dados atualizados com sucesso' }
+    else:
+        if request.method == 'POST':
+            if emailUnused:
+                # Se o e-mail não está em uso tiver algum dado inválido.
+                message = { 'type': 'danger', 'text': 'Dados inválidos' }
+            else:
+                # Se o e-mail já está em uso por outra pessoa.
+                message = { 'type': 'warning', 'text': 'E-mail já usado' }    
+
+
     context = {
         'usuarioForm': usuarioForm,
-        'userForm': userForm
+        'userForm': userForm,
+        'message': message
     }
     return render(request, template_name='usuario/usuario-edit.html', context=context, status=200)    
