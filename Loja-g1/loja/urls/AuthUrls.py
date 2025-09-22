23e4ad6d5be5b5a2b4940df5e39a663c8a616579
gasilvabr <gasilvabr@hotmail.com>
2025-09-22 15:30:19 -0300

    cap 10 loja g2

--- Loja-g1/loja/urls/AuthUrls.py
+++ Loja-g1/loja/urls/AuthUrls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from loja.views.AuthView import login_view
+from loja.views.AuthView import login_view, register_view
 
 urlpatterns = [
     path("login", login_view, name='login'),
+    # Adicione a linha a seguir
+    path("register", register_view, name='register'),    
 ]
